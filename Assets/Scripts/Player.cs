ea973ea02937627b56ff435c8eae1dbde72b346e
GoodLooser <127188082+GoodLooser@users.noreply.github.com>
2024-03-27 17:13:44 +0900

    무능한 코드

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 1f;
-    public float dashSpeed = 3f;
+    public float defaultspeed = 5f;
+    public float moveSpeed = 5f;
+    public float dashSpeed;
+    public float dashDuration = 0.5f; // 대쉬 지속 시간 (초 단위)
+    public float dashCoolTime = 1.5f; // 대쉬 쿨타임 (초 단위)
 
-
-    Rigidbody2D rigid2d;
-
-    Vector3 movement;
-    bool isDashing = false;
+    private Rigidbody2D rigid2d;
+    private Vector3 movement;
+    private bool isDashing = false;
+    private float dashCoolTimer = 0f; // 대쉬 쿨타임을 추적하기 위한 타이머
 
     private void Start()
     {
-        rigid2d = gameObject.GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        if (Input.GetButton("Dash"))
-        {
-            isDashing = true;
-        }
+        rigid2d = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
@@ -32,45 +26,43 @@ public class Player : MonoBehaviour
         Dash();
     }
 
-
     void Move()
     {
-        Vector3 moveVelocity= Vector3.zero;
-
-        if (Input.GetAxisRaw("Horizontal") < 0 )
-        {
-            moveVelocity = Vector3.down;
-
-        }
-
-        else if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            moveVelocity = Vector3.up;
-
-        }
-
-        else if (Input.GetAxisRaw("Vertical") > 0)
-        {
-            moveVelocity = Vector3.right;
-
-        }
-
-        else if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            moveVelocity = Vector3.left;
-
-        }
-
-        transform.position += moveVelocity * moveSpeed * Time.deltaTime;
-
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 moveVelocity = new Vector3(x, 0, z) * moveSpeed * Time.deltaTime;
+        transform.position += moveVelocity;
     }
 
     void Dash()
     {
-        Debug.Log("Dash!");
-        Debug.Log("Dash Cool Time Start");
+        if (Input.GetKey(KeyCode.LeftShift) && !isDashing && dashCoolTimer <= 0)
+        {
+            StartCoroutine(DoDash());
+            isDashing = true;
+        }
 
-        
+        // 대쉬 쿨타임 타이머 감소
+        if (dashCoolTimer > 0)
+        {
+            dashCoolTimer -= Time.deltaTime;
+        }
     }
 
+    IEnumerator DoDash()
+    {
+        moveSpeed *= 3; // 대쉬 속도 증가
+        yield return new WaitForSeconds(dashDuration);
+        moveSpeed /= 3; // 원래 이동 속도로 되돌림
+        Debug.Log("Dash Ended");
+        StartCoroutine(StartDashCoolTime());
+    }
+
+    IEnumerator StartDashCoolTime()
+    {
+        Debug.Log("Dash Cool Time Start");
+        dashCoolTimer = dashCoolTime;
+        yield return new WaitForSeconds(dashCoolTime);
+        isDashing = false;
+    }
 }
