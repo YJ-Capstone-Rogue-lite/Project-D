a9242f59948ea243a2701eec613ed0352928cb72
05 <145641517+HongYeongHo@users.noreply.github.com>
2024-04-03 19:46:08 +0900

    Ingame 애니메이션 추가

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     
-    public AudioSource musicsource;
+    public AudioSource[] musicsource;
 
 
     public void setMusicVolume(float volume)
     {
-        musicsource.volume = volume;
+        for(int i =0; i < musicsource.Length; i++)
+        {
+            musicsource[i].volume = volume;
+        }
     }
 
   
