c3eb8a7eb69c5cf3200360d34bcc8356ae997a18
05 <145641517+HongYeongHo@users.noreply.github.com>
2024-04-30 18:21:37 +0900

    Squashed commit of the following:
    commit f1e3b3df98866cbf5b09e6353273447e7d294b46
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Tue Apr 30 18:19:56 2024 +0900
    
        asdf
    
    commit 53fd110aa79b83387d0613e3a7ded56f39dda40a
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Tue Apr 30 14:53:05 2024 +0900
    
        add ingame TimeStop
    
    commit 141532762c43c0d11634fe1bdd17fafa2ceeba58
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Wed Apr 24 09:21:17 2024 +0900
    
        add Ingame Option base
    
    commit 16c4d856130e8b742374b55c9e0fff5b594b700c
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Tue Apr 23 20:26:38 2024 +0900
    
        modify UI
    
    commit f0bf43875fd7d6d07ab087022e291758656172ea
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Tue Apr 23 19:03:25 2024 +0900
    
        Title UI change
    
    commit 2be7e90cec7d69557a04fb3193c8b401d5f905c5
    Merge: 62aae12 5a6051d
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Tue Apr 23 16:27:57 2024 +0900
    
        Merge branch 'main' into Project-D_UI
    
    commit 62aae12fda0238ee09e86178d58af5deb53d2643
    Merge: 127153d fa26335
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Tue Apr 23 14:00:54 2024 +0900
    
        Merge branch 'main' into Project-D_UI
    
    commit 127153d4641d5474c488d809feea8507c631fb18
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Sun Apr 21 18:00:50 2024 +0900
    
        add Inventory UI base
    
    commit 6d6fa9bb700a33ff160417030e3c56eda9bab8d7
    Author: 05 <145641517+HongYeongHo@users.noreply.github.com>
    Date:   Sun Apr 21 17:23:01 2024 +0900
    
        Weapon Anim add

---
+++ Assets/Scripts/IngameUI/Minimap.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    [SerializeField] private GameObject Mini_Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
