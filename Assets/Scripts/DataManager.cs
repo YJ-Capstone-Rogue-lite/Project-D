cdf8dd3b54fcb899ace3f54ddd7c5b029c1ef26b
sonflea <dhk_1@naver.com>
2024-06-05 17:52:50 +0900

    save & load

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class DataManager : MonoBehaviour
 {
@@ -28,6 +30,11 @@ public class DataManager : MonoBehaviour
     // --- 저장용 클래스 변수 --- //
     public PlayerData data = new PlayerData();
 
+
+
+    public enum TEST { SAVE, LOAD };
+    public string id;
+    public string pw;
     
 
     // 불러오기
@@ -41,6 +48,7 @@ public class DataManager : MonoBehaviour
             // 저장된 파일 읽어오고 Json을 클래스 형식으로 전환해서 할당
             string FromJsonData = File.ReadAllText(filePath);
             data = JsonUtility.FromJson<PlayerData>(FromJsonData);
+            StartCoroutine(UnityWebRequestGETTest(TEST.LOAD));
             print("불러오기 완료");
             // 불러온 데이터 출력
         }
@@ -54,9 +62,42 @@ public class DataManager : MonoBehaviour
         string filePath = Application.persistentDataPath + "/" + GameDataFileName;
 
         // 이미 저장된 파일이 있다면 덮어쓰고, 없다면 새로 만들어서 저장
+        StartCoroutine(UnityWebRequestGETTest(TEST.SAVE));
         File.WriteAllText(filePath, ToJsonData);
-
         // 올바르게 저장됐는지 확인 (자유롭게 변형)
         print("저장 완료");
+    }
+
+    IEnumerator UnityWebRequestGETTest(TEST test)
+    {
+        WWWForm form = new WWWForm();
+        string url = "http://localhost:8181/ProjectD/";
+        form.AddField("id", id);
+        form.AddField("pw", pw);
+        form.AddField("playerdata", JsonUtility.ToJson(DataManager.Instance.data));
+        
+        switch(test)
+        {
+            case TEST.SAVE:
+                url += "DataSave.jsp";
+            break;
+            case TEST.LOAD:
+                url += "DataLoad.jsp";
+            break;
+        }
+
+        UnityWebRequest www = UnityWebRequest.Post(url, form);
+
+        yield return www.SendWebRequest();  // 응답이 올때까지 대기한다.
+
+        if (www.error == null)  // 에러가 나지 않으면 동작.
+        {
+            Debug.Log(www.downloadHandler.text);
+            if(test == TEST.LOAD) DataManager.Instance.data = JsonUtility.FromJson<PlayerData>(www.downloadHandler.text);
+        }
+        else
+        {
+            Debug.LogError("WebRequestException: " + www.error);
+        }
     }
 }
