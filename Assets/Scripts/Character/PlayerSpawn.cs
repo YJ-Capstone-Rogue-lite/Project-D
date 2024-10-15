a886853ba8af51577e1d8ae9d9d2fcdb2a4f23b1
sonflea <dhk_1@naver.com>
2024-10-15 17:14:04 +0900

    Update PlayerSpawn.cs

--- Assets/Scripts/Character/PlayerSpawn.cs
+++ Assets/Scripts/Character/PlayerSpawn.cs
@@ -9,14 +9,15 @@ public class PlayerSpawn : MonoBehaviour
     [SerializeField] private GameObject cameraInstance;
     void Start()
     {
-        var player = GameObject.FindWithTag("Player");
-        if(!player) this.player = player;
+        var player = GameObject.FindWithTag("Player").GetComponent<Character>();
+        if(!player) this.player = player.gameObject;
         player.transform.parent = null;
         player.transform.position = spawnPoint.transform.position;
+        player.m_shield = player.m_maxShield;
         if(!Camera.main)
         {
             var go = Instantiate(cameraInstance);
-            go.GetComponent<Camera_Player>().player = player;
+            go.GetComponent<Camera_Player>().player = player.gameObject;
             go.transform.position = player.transform.position;
         }
     }
